f47bd959c85f76c283c9ca4c3bae439ea5a690e6
gogagelkhauri <gogagelkhauri1@gmail.com>
2022-12-12 12:38:22 +0400

    changed cors

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://20.56.36.127:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://picnic-ui.herokuapp.com/');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
